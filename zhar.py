a13a2201db819f9b670e0e8233c0b5bb3ac584fd
amine10a <50719983+amine10a@users.noreply.github.com>
2019-11-23 07:37:58 +0100

    Add files via upload

---
+++ zhar.py
@@ -0,0 +1,115 @@
+red, WHITE, CYAN, GREEN, DEFAULT = '\033[91m', '\033[46m', '\033[36m', '\033[1;32m',  '\033[0m'
+print('---------------------')
+print(CYAN,'| 1:🔒  2:🔒   3:🔒 |')
+print(CYAN,'| 3:🔒  4:🔒   5:🔒 |')
+print(CYAN,'| 6:🔒  7:🔒   8:🔒 |',DEFAULT)
+
+x = int(input('enter your choise>> '))
+if x == 6 :print(GREEN,'you win!!🏅',DEFAULT) 
+if x == 1 :print(red,'you lost😢',DEFAULT)
+if x == 2 :print(red,'you lost😢',DEFAULT)
+if x == 3 :print(red,'you lost😢',DEFAULT)
+if x == 4 :print(red,'you lost😢',DEFAULT)
+if x == 5 :print(red,'you lost😢',DEFAULT)
+if x == 7 :print(red,'you lost😢',DEFAULT)
+if x == 8 :print(red,'you lost😢',DEFAULT)
+m = input('do you have continue y/n==> ')
+if m == 'n' :exit()
+if m == 'y' :
+    print('---------------------')
+    print(CYAN,'| 1:🔒  2:🔒   3:🔒 |')
+    print(CYAN,'| 3:🔒  4:🔒   5:🔒 |')
+    print(CYAN,'| 6:🔒  7:🔒   8:🔒 |',DEFAULT)
+
+
+y = int(input('enter your choit>> '))
+if y == 7 :print(GREEN,'you win!!🏅',DEFAULT) 
+if y == 1 :print(red,'you lost😢',DEFAULT)
+if y == 2 :print(red,'you lost😢',DEFAULT)
+if y == 3 :print(red,'you lost😢',DEFAULT)
+if y == 4 :print(red,'you lost😢',DEFAULT)
+if y == 5 :print(red,'you lost😢',DEFAULT)
+if y == 6 :print(red,'you lost😢',DEFAULT)   
+if y == 8 :print(red,'you lost😢',DEFAULT)
+
+m = input('do you have continue y/n==> ')
+if m == 'n' :exit()
+if m == 'y' :
+    print('---------------------')
+    print(CYAN,'| 1:🔒  2:🔒   3:🔒 |')
+    print(CYAN,'| 3:🔒  4:🔒   5:🔒 |')
+    print(CYAN,'| 6:🔒  7:🔒   8:🔒 |',DEFAULT)
+
+
+y = int(input('enter your choit>> '))
+if y == 4 :print(GREEN,'you win!!🏅',DEFAULT) 
+if y == 1 :print(red,'you lost😢',DEFAULT)
+if y == 2 :print(red,'you lost😢',DEFAULT)
+if y == 3 :print(red,'you lost😢',DEFAULT)
+if y == 7 :print(red,'you lost😢',DEFAULT)
+if y == 5 :print(red,'you lost😢',DEFAULT)
+if y == 6 :print(red,'you lost😢',DEFAULT)   
+if y == 8 :print(red,'you lost😢',DEFAULT)
+
+m = input('do you have continue y/n==> ')
+if m == 'n' :exit()
+if m == 'y' :
+    print('---------------------')
+    print(CYAN,'| 1:🔒  2:🔒   3:🔒 |')
+    print(CYAN,'| 3:🔒  4:🔒   5:🔒 |')
+    print(CYAN,'| 6:🔒  7:🔒   8:🔒 |',DEFAULT)
+
+
+y = int(input('enter your choit>> '))
+if y == 1 :print(GREEN,'you win!!🏅',DEFAULT) 
+if y == 7 :print(red,'you lost😢',DEFAULT)
+if y == 2 :print(red,'you lost😢',DEFAULT)
+if y == 3 :print(red,'you lost😢',DEFAULT)
+if y == 4 :print(red,'you lost😢',DEFAULT)
+if y == 5 :print(red,'you lost😢',DEFAULT)
+if y == 6 :print(red,'you lost😢',DEFAULT)   
+if y == 8 :print(red,'you lost😢',DEFAULT)
+
+m = input('do you have continue y/n==> ')
+if m == 'n' :exit()
+if m == 'y' :
+    print('---------------------')
+    print(CYAN,'| 1:🔒  2:🔒   3:🔒 |')
+    print(CYAN,'| 3:🔒  4:🔒   5:🔒 |')
+    print(CYAN,'| 6:🔒  7:🔒   8:🔒 |',DEFAULT)
+
+
+y = int(input('enter your choit>> '))
+if y == 8 :print(GREEN,'you win!!🏅',DEFAULT) 
+if y == 1 :print(red,'you lost😢',DEFAULT)
+if y == 2 :print(red,'you lost😢',DEFAULT)
+if y == 3 :print(red,'you lost😢',DEFAULT)
+if y == 4 :print(red,'you lost😢',DEFAULT)
+if y == 5 :print(red,'you lost😢',DEFAULT)
+if y == 6 :print(red,'you lost😢',DEFAULT)   
+if y == 7 :print(red,'you lost😢',DEFAULT)
+
+m = input('do you have continue y/n==> ')
+if m == 'n' :exit()
+if m == 'y' :
+    print('---------------------')
+    print(CYAN,'| 1:🔒  2:🔒   3:🔒 |')
+    print(CYAN,'| 3:🔒  4:🔒   5:🔒 |')
+    print(CYAN,'| 6:🔒  7:🔒   8:🔒 |',DEFAULT)
+
+
+y = int(input('enter your choit>> '))
+if y == 7 :print(GREEN,'you win!!🏅',DEFAULT) 
+if y == 1 :print(red,'you lost😢',DEFAULT)
+if y == 2 :print(red,'you lost😢',DEFAULT)
+if y == 3 :print(red,'you lost😢',DEFAULT)
+if y == 4 :print(red,'you lost😢',DEFAULT)
+if y == 5 :print(red,'you lost😢',DEFAULT)
+if y == 6 :print(red,'you lost😢',DEFAULT)   
+if y == 8 :print(red,'you lost😢',DEFAULT)
+
+
+
+exit()
+
+
